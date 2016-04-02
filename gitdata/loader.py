91f6492510d536d38f12c3a81a21f65f55cb01f4
Trofimov Igor <mister.larpy@ya.ru>
2016-04-02 14:22:49 +0500

    Write comments and annotations in loader.py

--- gitdata/loader.py
+++ gitdata/loader.py
@@ -13,7 +13,16 @@ class GitHubFinder:
     """
 
     @classmethod
-    def find_module(cls, fullname, path=None):
+    def find_module(cls, fullname: str, path: None = None) -> object:
+        """
+        Find our module
+
+        :param path: Nothing interesting
+        :type fullname: str
+        :param fullname: Module's full name in search
+        :rtype: GitHubLoader class
+        :return: GitHubLoader class
+        """
         name = fullname.split('.')
         if 'gitdata' in name and 'github' in name:
             return GitHubLoader
@@ -25,9 +34,19 @@ class GitHubLoader:
     """
 
     @classmethod
-    def load_module(cls, fullname):
+    def load_module(cls, fullname: str) -> object:
+        """
+        Load our module
+
+        :type fullname: str
+        :param fullname: Module's full name in search
+        :rtype: types.ModuleType or Worker class
+        :return: module
+        """
         name = fullname.split('.')
 
+        if 'gitdata' not in name or 'github' not in name:
+            raise ImportError('GitHubLoader: bad module name {0}'.format(fullname))
         if len(name) < 4:
             module = types.ModuleType(fullname)
             module.__file__ = 'github_db:' + fullname
@@ -35,11 +54,12 @@ class GitHubLoader:
             module.__loader__ = GitHubLoader
             sys.modules.setdefault(fullname, module)
             return module
-        else:
+        elif len(name) == 4:
             from . import worker
-            w = worker.Worker(name[2], name[3])
-            sys.modules.setdefault(fullname, w)
-            return w
+            module = worker.Worker(name[2], name[3])
+            sys.modules.setdefault(fullname, module)
+            return module
+        raise ImportError('GitHubLoader: bad module name {0}'.format(fullname))
 
 
 sys.meta_path.append(GitHubFinder)
