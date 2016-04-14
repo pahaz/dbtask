3b3c299d77b686cc87fb6ac15f997bd208054058
LeMarck <jeysonep@gmail.com>
2016-04-14 18:54:04 +0500

    Update gitimport.py

--- gitimport.py
+++ gitimport.py
@@ -2,37 +2,45 @@ import json
 import os
 import sys
 import types
+from subprocess import Popen
 
 __all__ = ['git_import']
 
 
 class GitHubModule:
     def __init__(self, user, repo):
-        self.user = user
-        self.repo = repo
+        self._user = user
+        self._repo = repo
         self._clone()
 
     def _clone(self):
         self.repo_root = os.path.join(os.getcwd(), "github_modules")
-        self.repo_dir_name = self.user + "/" + self.repo
+        if not os.path.exists(self.repo_root):
+            os.mkdir(self.repo_root)
+        self.repo_dir_name = self._user + "/" + self._repo
         self.repo_dir = os.path.join(self.repo_root, self.repo_dir_name)
         if os.path.exists(self.repo_dir):
-            os.system("cd %s && git pull -q" % self.repo_dir)
-
+            code = self._pull()
         else:
-            os.mkdir(self.repo_root)
-            os.system(
+            code = Popen(
                 "git clone https://github.com/%s/%s.git %s -q"
-                % (self.user, self.repo, self.repo_dir),
-            )
+                % (self._user, self._repo, self.repo_dir)).wait()
+        if code != 0:
+            raise ImportError('ImportWithGitHub: bad module name %s.%s' % (self._user, self._repo))
+
+    def _pull(self):
+        if Popen("git -C %s fetch --all -q" % self.repo_dir).wait() == 0:
+            if Popen("git -C %s reset --hard origin/master -q" % self.repo_dir).wait() == 0:
+                return Popen("git -C %s pull -q" % self.repo_dir).wait()
+        return 404
 
     def load(self, names):
-        os.system("cd %s && git pull -q" % self.repo_dir)
-        for name in names:
-            file_name = "%s/%s.json" % (self.repo_dir, name)
-            if os.path.exists(file_name):
-                with open(file_name, 'r') as file:
-                    self.__setattr__(name, json.loads(file.read()))
+        if self._pull() == 0:
+            for name in names:
+                file_name = "%s/%s.json" % (self.repo_dir, name)
+                if os.path.exists(file_name):
+                    with open(file_name, 'r') as file:
+                        self.__setattr__(name, json.loads(file.read()))
 
 
 class ImportWithGitHub(object):
